85fb0826aa79817fc12a50da230fa4fbb3c6593b
Mehran Abdi <mehranabdi13793@gmail.com>
2023-12-24 19:25:33 +0330

    Solve factorial trailing zeroes

---
+++ Factorial Trailing Zeroes/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine(TrailingZeroes(125));
+
+int TrailingZeroes(int n)
+{
+    int result = 0;
+    while (n > 0)
+    {
+        result += (n / 5);
+        n = n / 5;
+    }
+    return result;
+}
