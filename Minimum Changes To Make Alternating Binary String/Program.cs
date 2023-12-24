fe0ce9dc93dc8fdf6996610ae0ddfc6ca990d329
Mehran Abdi <mehranabdi13793@gmail.com>
2023-12-24 18:27:25 +0330

    Add project files.

---
+++ Minimum Changes To Make Alternating Binary String/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine(MinOperations("01"));
+
+int MinOperations(string s)
+{
+    var secondCounter = 0;
+    var counter = 0;
+    char previous = s[0];
+    for (int i = 1; i < s.Length; i++)
+    {
+        if (s[i] == previous)
+        {
+            counter++;
+            previous = previous == '0' ? '1' : '0';
+        }
+        else
+            previous = s[i];
+    }
+
+    previous = s.Last();
+
+    for (int i = s.Length - 2; i >= 0; i--)
+    {
+        if (s[i] == previous)
+        {
+            secondCounter++;
+            previous = previous == '0' ? '1' : '0';
+        }
+        else
+            previous = s[i];
+    }
+    return counter > secondCounter ? counter : secondCounter;
+}
