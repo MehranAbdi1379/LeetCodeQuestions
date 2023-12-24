b855b8eb6144d8eb86c78f246203fd3e0a2f659e
Mehran Abdi <mehranabdi13793@gmail.com>
2023-12-24 18:51:12 +0330

    Solve merge sorted array question

---
+++ Merge Sorted Array/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Merge(new int[] { 1, 2, 3, 0, 0, 0 }, 3, new int[] { 2, 5, 6 }, 3);
+
+void Merge(int[] nums1, int m, int[] nums2, int n)
+{
+    for (int i = m + n - 1; i >= 0; i--)
+    {
+        if (n > 0 && m > 0 && nums1[m - 1] > nums2[n - 1])
+        {
+            nums1[i] = nums1[m - 1];
+            m--;
+        }
+        else if (n > 0)
+        {
+            nums1[i] = nums2[n - 1];
+            n--;
+        }
+    }
+
+    foreach (var num in nums1)
+    {
+        Console.WriteLine(num);
+    }
+}
